f614d4deb77f2838cfdacaa00551baae10f5371f
Daniel Brain <github@biboop.com>
2011-11-10 06:32:09 +1100

    Connect add / edit up

--- src/com/shinetech/android/slyncy/SlyncyActivity.java
+++ src/com/shinetech/android/slyncy/SlyncyActivity.java
@@ -1,26 +1,65 @@
 package com.shinetech.android.slyncy;
 
+import java.util.Map;
+
+import org.codehaus.jackson.JsonNode;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
+import org.ektorp.DocumentNotFoundException;
+import org.ektorp.ReplicationCommand;
+import org.ektorp.ViewQuery;
+import org.ektorp.ViewResult.Row;
 import org.ektorp.android.http.AndroidHttpClient;
+import org.ektorp.android.util.CouchbaseViewListAdapter;
+import org.ektorp.android.util.EktorpAsyncTask;
+import org.ektorp.impl.StdCouchDbInstance;
+import org.ektorp.support.DesignDocument;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.couchbase.android.CouchbaseMobile;
 import com.couchbase.android.ICouchbaseDelegate;
 
-public class SlyncyActivity extends Activity implements android.view.View.OnClickListener {
+public class SlyncyActivity extends Activity implements OnClickListener {
+	// request codes
+	private static final int ADD_ITEM = 1;
+	private static final int EDIT_ITEM = 2;
+	
+	// db constants
+	protected static final String DATABASE_NAME = "slyncy";
+	protected static final String designDocId = "_design/slyncy";
+	protected static final String byNameViewName = "byName";
+	// TODO Ektorp list adapter changes feed currently doesn't includeDocs, should be an option.
+	protected static final String byNameViewFn = "function(doc) {if (doc.name) emit(doc.name, doc);}";
 	
 	protected static ServiceConnection couchConnection;
 	protected static AndroidHttpClient httpClient;
+	protected static CouchDbInstance dbInstance;
+	protected ReplicationCommand pushReplicationCommand;
+	protected ReplicationCommand pullReplicationCommand;
+	protected CouchDbConnector dbConnector;
+	protected CouchbaseViewListAdapter adapter;
+	
+	private Dialog loadingDialog;
 	
     /** Called when the activity is first created. */
     @Override
@@ -31,6 +70,7 @@ public class SlyncyActivity extends Activity implements android.view.View.OnClic
         addItemButton.setOnClickListener(this);
         ListView itemsListView = (ListView)findViewById(R.id.item_list);
         registerForContextMenu(itemsListView);
+        showLoading();
         startCouch();
     }
     
@@ -39,9 +79,52 @@ public class SlyncyActivity extends Activity implements android.view.View.OnClic
     	int viewId = v.getId();
     	switch (viewId) {
     	case R.id.new_item_button:
-    		startActivity(new Intent(this, SlyncyAddItemActivity.class));
+    		startActivityForResult(new Intent(this, SlyncyAddItemActivity.class), ADD_ITEM);
     		break;
     	}
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	if (resultCode == RESULT_OK) {
+    		switch (requestCode) {
+        	case ADD_ITEM:
+        		Map<String, String> addValues = (Map<String, String>)data.getSerializableExtra(SlyncyAddItemActivity.FORM_VALUES);
+        		saveDocument(addValues, "CREATE");
+        		break;
+        	case EDIT_ITEM:
+        		Map<String, String> editValues = (Map<String, String>)data.getSerializableExtra(SlyncyAddItemActivity.FORM_VALUES);
+        		saveDocument(editValues, "EDIT");
+        		break;
+    		}
+    	} else {
+    		super.onActivityResult(requestCode, resultCode, data);
+    	}
+    }
+    
+    public void saveDocument(final Map<String, String> values, final String mode) {
+    	new EktorpAsyncTask() {	
+			@Override
+			protected void doInBackground() {
+				CouchDbConnector connector = dbInstance.createConnector(DATABASE_NAME, true);
+				if (mode.equals("CREATE")) {
+					connector.create(values);
+				} else if (mode.equals("EDIT")) {
+					connector.update(values);
+				}
+			}
+		}.execute();
+    }
+    
+    public void deleteDocument(final String id, final String rev) {
+    	new EktorpAsyncTask() {
+			@Override
+			protected void doInBackground() {
+				Log.d("CouchDb", String.format("Deleting %s rev: %s", id, rev));
+				CouchDbConnector connector = dbInstance.createConnector(DATABASE_NAME, true);
+				connector.delete(id, rev);
+			}
+		}.execute();
     }
     
     @Override
@@ -54,6 +137,7 @@ public class SlyncyActivity extends Activity implements android.view.View.OnClic
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()) {
     	case R.id.start_replication:
+    		startReplication();
     		return true;
     	case R.id.settings:
     		startActivity(new Intent(this, SlyncyPreferenceActivity.class));
@@ -65,12 +149,25 @@ public class SlyncyActivity extends Activity implements android.view.View.OnClic
     
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-    	super.onCreateContextMenu(menu, v, menuInfo);
+    	getMenuInflater().inflate(R.menu.item_menu, menu);
     }
     
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-    	return super.onContextItemSelected(item);
+    	AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+    	switch (item.getItemId()) {
+    		case R.id.edit_item:
+    			Intent editIntent = new Intent(getApplicationContext(), SlyncyAddItemActivity.class);
+    			editIntent.putExtra(SlyncyAddItemActivity.EDIT_DOC, adapter.getRow(menuInfo.position).getValue());
+    			startActivityForResult(editIntent, EDIT_ITEM);
+    			return true;
+    		case R.id.delete_item:
+    			Row row = adapter.getRow(menuInfo.position);
+    			deleteDocument(row.getId(), row.getValueAsNode().get("_rev").getTextValue());
+    			return true;
+			default:
+				return super.onContextItemSelected(item);
+    	}
     }
     
     private void startCouch() {
@@ -79,23 +176,121 @@ public class SlyncyActivity extends Activity implements android.view.View.OnClic
     }
     
     private void startEktorp(String host, int port) {
+    	if (httpClient != null) httpClient.shutdown();
+    	AndroidHttpClient.Builder clientBuilder = new AndroidHttpClient.Builder();
+    	httpClient = (AndroidHttpClient)clientBuilder.host(host).port(port).build();
+    	dbInstance = new StdCouchDbInstance(httpClient);
     	
+    	new EktorpAsyncTask() {
+			@Override
+			protected void doInBackground() {
+				dbConnector = dbInstance.createConnector(DATABASE_NAME, true);
+
+				try {
+					DesignDocument dDoc = dbConnector.get(DesignDocument.class, designDocId);
+					dDoc.addView(byNameViewName, new DesignDocument.View(byNameViewFn));
+					dbConnector.update(dDoc);
+				} catch (DocumentNotFoundException ndfe) {
+					DesignDocument dDoc = new DesignDocument(designDocId);
+					dDoc.addView(byNameViewName, new DesignDocument.View(byNameViewFn));
+					dbConnector.create(dDoc);
+				}
+			}
+			
+			@Override
+			protected void onSuccess() {
+				ViewQuery query = new ViewQuery().designDocId(designDocId).viewName(byNameViewName).descending(true);
+				adapter = new CouchbaseViewListAdapter(dbConnector, query, true) {
+					@Override
+					public View getView(int position, View currentView, ViewGroup parent) {
+						View view = currentView;
+				        if (view == null) {
+				            view = getLayoutInflater().inflate(R.layout.list_item, null);
+				        }
+				        
+				        TextView nameText = (TextView)view.findViewById(R.id.nameText);
+				        Row row = getRow(position);
+				        JsonNode item = row.getValueAsNode();
+				        JsonNode itemText = item.get("name");
+				        if (itemText != null) {
+				        	nameText.setText(itemText.getTextValue());
+				        } else {
+				        	nameText.setText("");
+				        }
+				        view.setId(position);
+				        view.setTag(row.getId());
+				        return view;
+					}
+				};
+				ListView itemList = (ListView)findViewById(R.id.item_list);
+				itemList.setAdapter(adapter);
+				stopLoading();
+			}
+		}.execute();
     }
     
     private ICouchbaseDelegate couchDelegate = new ICouchbaseDelegate() {
 		@Override
 		public void exit(String error) {
-			
+			error = String.format("Failed to connect to couch: %s", error);
+			Log.e("CouchDb", error);
+			Toast.makeText(getBaseContext(), error, Toast.LENGTH_LONG).show();
 		}
 		
 		@Override
 		public void couchbaseStarted(String host, int port) {
-			
+			startEktorp(host, port);
 		}
 	};
 	
 	protected void onDestroy() {
 		if (couchConnection != null) unbindService(couchConnection);
+		if (adapter != null) adapter.cancelContinuous();
 		if (httpClient != null) httpClient.shutdown();
+		super.onDestroy();
 	};
+	
+	private void startReplication() {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		String syncUrl = prefs.getString("couchdb_url_pref", null);
+
+		pushReplicationCommand = new ReplicationCommand.Builder()
+			.source(DATABASE_NAME)
+			.target(syncUrl)
+			.continuous(true)
+			.build();
+
+		EktorpAsyncTask pushReplication = new EktorpAsyncTask() {
+			@Override
+			protected void doInBackground() {
+				dbInstance.replicate(pushReplicationCommand);
+			}
+		};
+		pushReplication.execute();
+
+		pullReplicationCommand = new ReplicationCommand.Builder()
+			.source(syncUrl)
+			.target(DATABASE_NAME)
+			.continuous(true)
+			.build();
+
+		EktorpAsyncTask pullReplication = new EktorpAsyncTask() {
+			@Override
+			protected void doInBackground() {
+				dbInstance.replicate(pullReplicationCommand);
+			}
+		};
+		pullReplication.execute();
+	}
+	
+	public void showLoading() {
+		loadingDialog = new Dialog(this);
+		loadingDialog.setTitle("Starting Couch");
+		loadingDialog.show();
+	}
+	
+	public void stopLoading() {
+		loadingDialog.dismiss();
+		loadingDialog = null;
+	}
 }
--- src/com/shinetech/android/slyncy/SlyncyAddItemActivity.java
+++ src/com/shinetech/android/slyncy/SlyncyAddItemActivity.java
@@ -1,13 +1,29 @@
 package com.shinetech.android.slyncy;
 
+import java.util.LinkedHashMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SlyncyAddItemActivity extends Activity implements OnClickListener {
+	public static final int ADD_ITEM = 1;
+	public static final int EDIT_ITEM = 2;
+	public static final String EDIT_DOC = "EDIT_DOC";
+	public static final String FORM_VALUES = "FORM_VALUES";
+	private String currentId;
+	private String currentRev;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +33,69 @@ public class SlyncyAddItemActivity extends Activity implements OnClickListener {
 		Button doneButton = (Button)findViewById(R.id.done_button);
 		addFieldButton.setOnClickListener(this);
 		doneButton.setOnClickListener(this);
+		onNewIntent(getIntent());
 	}
 	
-	public void addNewField() {
+	@Override
+	protected void onNewIntent(Intent intent) {
+		currentId = null;
+		currentRev = null;
+		String editItem = intent.getStringExtra(EDIT_DOC);
+		if (editItem != null) {
+			try {
+				JSONObject item = new JSONObject(editItem);
+				JSONArray itemNames = item.names();
+				int itemNamesLength = itemNames.length();
+				for (int i = 0; i < itemNamesLength; i++) {
+					String itemName = itemNames.getString(i);
+					String value = item.getString(itemName);
+					if (itemName.equals("name")) {
+						TextView nameView = (TextView)findViewById(R.id.name_field);
+						nameView.setText(value);
+					} else if (itemName.equals("_id")) {
+						currentId = value;
+					} else if (itemName.equals("_rev")) {
+						currentRev = value;
+					} else {
+						addNewField(itemName, value);
+					}
+				}
+			} catch (Exception e) {
+				// We don't care, its an example app
+				Log.e("Json", "Error parsing object JSON", e);				
+			}
+		}
+		super.onNewIntent(intent);
+	}
+	
+	public void addNewField(String name, String value) {
 		ViewGroup fieldContainer = (ViewGroup)findViewById(R.id.field_container);
-		getLayoutInflater().inflate(R.layout.new_field, fieldContainer);
+		ViewGroup addedView = (ViewGroup)getLayoutInflater().inflate(R.layout.new_field, null);
+		if (name != null) {
+			TextView nameView = (TextView)addedView.getChildAt(0);
+			nameView.setText(name);
+		}
+		if (value != null) {
+			TextView valueView = (TextView)addedView.getChildAt(1);
+			valueView.setText(value);
+		}
+		fieldContainer.addView(addedView);
+	}
+	
+	public LinkedHashMap<String, String> getValues() {
+		ViewGroup fieldContainer = (ViewGroup)findViewById(R.id.field_container);
+		int numberOfFields = fieldContainer.getChildCount();
+		LinkedHashMap<String, String> values = new LinkedHashMap<String, String>(numberOfFields);
+		for (int i = 0; i < numberOfFields; i++) {
+			LinearLayout childView = (LinearLayout)fieldContainer.getChildAt(i);
+			// No sanity checking at all, like a boss
+			TextView nameText = (TextView)childView.getChildAt(0);
+			EditText valueText = (EditText)childView.getChildAt(1);
+			values.put(nameText.getText().toString(), valueText.getText().toString());
+		}
+		if (currentId != null) values.put("_id", currentId);
+		if (currentRev != null) values.put("_rev", currentRev);
+		return values;
 	}
 	
 	@Override
@@ -29,10 +103,12 @@ public class SlyncyAddItemActivity extends Activity implements OnClickListener {
 		int viewId = v.getId();
 		switch (viewId) {
 		case R.id.add_field_button:
-			addNewField();
+			addNewField(null, null);
 			break;
 		case R.id.done_button:
-			setResult(RESULT_OK);
+			Intent data = new Intent();
+			data.putExtra(FORM_VALUES, getValues());
+			setResult(RESULT_OK, data);
 			finish();
 			break;
 		}
